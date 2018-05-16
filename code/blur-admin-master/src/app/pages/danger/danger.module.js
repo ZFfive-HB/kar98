23074d425956657dc7104d03ebbf584c6a2da7dc
mac <bruce870827@gmail.com>
2018-05-16 17:54:02 +0800

    危险作业未完成，有错误版本

--- code/blur-admin-master/src/app/pages/danger/danger.module.js
+++ code/blur-admin-master/src/app/pages/danger/danger.module.js
@@ -15,8 +15,8 @@
           url: '/danger',
           abstract:true,
           title: '危险作业管理',
-          // templateUrl: 'app/pages/profile/profile.html',
-          // controller: 'ProfilePageCtrl',
+          template: '<ui-view autoscroll="true" autoscroll-body-top></ui-view>',
+          controller: 'dangerPageCtrl',
           sidebarMeta: {
             icon: 'ion-grid',
             order: 300
@@ -24,7 +24,7 @@
         }).state('danger.fire', {
           url: '/fire',
           title: '动火作业',
-          // templateUrl: 'app/pages/profile/profile.html',
+          templateUrl: 'app/pages/danger/fire/fire.html',
           // controller: 'ProfilePageCtrl',
           sidebarMeta: {
             icon: 'ion-grid',
