c1fac2659b2c726939b22ba23ff1445c48319817
mengxiaoming <18271917@qq.com>
2018-09-15 13:09:42 +0800

    国家法律

--- code/blur-admin-master/dev-release/app/pages/laws/nationLaws/nationLaws.js
+++ code/blur-admin-master/dev-release/app/pages/laws/nationLaws/nationLaws.js
@@ -9,147 +9,89 @@
         .controller('nationLawsCtrl', nationLawsCtrl);
 
     /** @ngInject */
-    function nationLawsCtrl($scope, $filter, editableOptions, editableThemes,$location) {
-        // $scope.smartTablePageSize = 20;
-
-
-        // $scope.editableTableData = $scope.smartTableData.slice(0, 36);
-
-        $scope.nationLaws = [
-            {
-                "id": 1,
-                "file_number":"主席令第30号",
-                "name": "中华人民共和国刑法修正案(九)",
-                "organization":"全国人大",
-                "date":"2018-11-01",
-                "status": 4,
-                "group": 3
-            },
-            {
-                "id": 2,
-                "file_number":"根据2011年12月13日第十一届全国人民代表大会常务委员会《关于修改》",
-                "name": "中华人民共和国职业病防治法",
-                "organization":"人大常委会",
-                "date":"2011-12-31",
-                "status": 3,
-                "group": 1
-            },
-            {
-                "id": 3,
-                "file_number":"国家安全生产监督管理总局令2007年第11号",
-                "name": "注册安全工程师管理规定",
-                "organization":"国家安全生产监督管理总局",
-                "date":"2016-07-20",
-                "status": 3,
-                "group": 2
-            },
-            {
-                "id": 4,
-                "file_number":"主席令第4号 ",
-                "name": "中华人民共和国循环经济促进法",
-                "organization":"人大常委会",
-                "date":"2009-01-01",
-                "group": 4
-            },
-            {
-                "id": 5,
-                "file_number":"主席令第6号 ",
-                "name": "中华人民共和国消防法(2008修订)",
-                "organization":"人大常委会",
-                "date":"2009-05-01",
-                "status": 1,
-                "group": 1
-            },
-            {
-                "id": 6,
-                "file_number":"主席令第30号",
-                "name": "中华人民共和国刑法修正案(九)",
-                "organization":"全国人大",
-                "date":"2018-11-01",
-                "status": 4,
-                "group": 2
-            },
-            {
-                "id": 7,
-                "file_number":"主席令第30号",
-                "name": "中华人民共和国刑法修正案(九)",
-                "organization":"全国人大",
-                "date":"2018-11-01",
-                "status": 4,
-                "group": 1
-            },
-            {
-                "id": 8,
-                "file_number":"主席令第30号",
-                "name": "中华人民共和国刑法修正案(九)",
-                "organization":"全国人大",
-                "date":"2018-11-01",
-                "status": 4,
-                "group": 2
-            },
-            {
-                "id": 9,
-                "file_number":"主席令第30号",
-                "name": "中华人民共和国刑法修正案(九)",
-                "organization":"全国人大",
-                "date":"2018-11-01",
-                "status": 1,
-                "group": 2
-            },
-            {
-                "id": 10,
-                "file_number":"主席令第30号",
-                "name": "中华人民共和国刑法修正案(九)",
-                "organization":"全国人大",
-                "date":"2018-11-01",
-                "status": 1,
-                "group": 3
-            },
-        ];
-
-
-
-        // $scope.removeUser = function(index) {
-        //   $scope.users.splice(index, 1);
-        // };
-
+    function nationLawsCtrl($scope, $filter, editableOptions, editableThemes,$location, $http, $uibModal, dataServiceURL, toastr) {
+        $scope.nationLawsPageSize = 10;
+        $scope.queryLaw = function(){
+            var url = dataServiceURL+"nationLaws/getLaws";
+            //var url = "app/pages/laws/nationLaws/nationLaws.json";
+            $http.get(url).success(function (param) {
+                $scope.nationLawsArray = param;
+            }).error(function (err) {
+                // 如果服务端请求失败则调用本地静态数据
+                console.log('服务器连接失败，请检查网络' + err);
+            })
+        };
+       
         $scope.addLaw = function() {
-            $scope.inserted = {
-                id: $scope.nationLaws.length+1,
-                file_number:"",
-                name: "",
-                organization:"",
-                date:"",
-                status: null,
-                group: null
-            };
-            $scope.nationLaws.push($scope.inserted);
+            var modalInstance = $uibModal.open({
+                animation: true,
+                controller: 'nationLawsAddModalPageCtrl',
+                templateUrl: 'app/pages/laws/nationLaws/nationLawsAdd.html',
+                size: ''
+            });
+            modalInstance.result.then(function (result) {
+                // 在回调函数中写入新节点
+                if (result) {
+                    //$scope.nationLaws.push(angular.copy(result));
+                    $scope.queryLaw();
+                }
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
         };
+
         // 跳转编辑页面
-        $scope.editLaw = function(){
-            // $location.path();
-            console.log(
-                $('#editContent')
-            );
+        $scope.editLaw = function(index){
+            var modalInstance = $uibModal.open({
+                animation: true,
+                controller: 'nationLawsEditModalPageCtrl',
+                templateUrl: 'app/pages/laws/nationLaws/nationLawsEdit.html',
+                size: '',
+                resolve: {
+                    items: function () {
+                      return $scope.nationLawsArray[index];
+                    }
+                }
+            });
+            modalInstance.result.then(function () {
+                $scope.queryLaw();
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
         };
+
+        $scope.viewLaw = function(index){
+            var modalInstance = $uibModal.open({
+                animation: true,
+                controller: 'nationLawsViewModalPageCtrl',
+                templateUrl: 'app/pages/laws/nationLaws/nationLawsView.html',
+                size: '',
+                resolve: {
+                    items: function () {
+                      return $scope.nationLawsArray[index];
+                    }
+                }
+            });
+        };
+
         // 跳转删除页面
-        $scope.editLaw = function(){
-            // $location.path();
-            console.log(
-                $('#editContent')
-            );
+        $scope.removeLaw = function(index, id) {
+            $http.post(dataServiceURL+"nationLaws/deleteLaw?id="+id).success(function () {
+                toastr.success('删除成功!');
+                $scope.nationLawsArray.splice(index, 1);
+                $scope.queryLaw();
+            }).error(function (err) {
+                console.log('服务器连接失败，请检查网络' + err);
+            })
         };
+
         // 上传附件
         $scope.upload = function () {
             var fileInput = document.getElementById('uploadFile');
             fileInput.click();
-      
-          };
-        editableOptions.theme = 'bs3';
-        editableThemes['bs3'].submitTpl = '<button type="submit" class="btn btn-primary btn-with-icon"><i class="ion-checkmark-round"></i></button>';
-        editableThemes['bs3'].cancelTpl = '<button type="button" ng-click="$form.$cancel()" class="btn btn-default btn-with-icon"><i class="ion-close-round"></i></button>';
+        };
 
-
+        $scope.queryLaw();
     }
 
 })();
